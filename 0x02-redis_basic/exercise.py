4f588e4c5fe60ad628e1571f66f46675d031e412
Kimberley-saka <kimberleysaka@gmail.com>
2023-07-19 16:04:16 +0000

    updated

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -24,3 +24,31 @@ class Cache:
         if isinstance(data, (str, bytes, int, float)):
             self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) ->
+    /Union[str, bytes, int, None]:
+        """
+        converts the data back to the desired format
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+
+        if fn is None:
+            return data
+
+        return fn(data)
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        automatically parametrize Cache.get with the correct
+        conversion function
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        automatically parametrize Cache.get with the correct
+        conversion function
+        """
+        return self.get(key, fn=lambda d: int(d))
