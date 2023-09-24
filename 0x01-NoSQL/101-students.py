ca81942b83a24e6b7d168f8b97458a7ef8f47bb5
Kimberley-saka <kimberleysaka@gmail.com>
2023-09-24 06:36:40 -0700

    solutions to tasks

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Sorting
+"""
+
+
+def top_students(mongo_collection):
+    """ sort students by score """
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
